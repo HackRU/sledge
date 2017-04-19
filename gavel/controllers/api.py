308a43d0475c86960c16ade5d5be2023f871747f
Heman Gandhi <hemangandhi@gmail.com>
2017-04-18 23:39:13 -0400

    On Tue Apr 18 23:39:13 EDT 2017: Added a querying thing for the hackerview

--- gavel/controllers/api.py
+++ gavel/controllers/api.py
@@ -31,6 +31,10 @@ def annotator_dump():
     ] for a in annotators]
     return Response(utils.data_to_csv_string(data), mimetype='text/csv')
 
+def get_seen_ct(item_id):
+    return sum(1 if d.loser_id == item_id or d.winner_id == item_id else 0\
+               for d in Decision.query.all())
+
 @app.route('/api/decisions.csv')
 @utils.requires_auth
 def decisions_dump():
@@ -42,4 +46,12 @@ def decisions_dump():
         str(d.loser.id),
         str(d.time)
     ] for d in decisions]
+    return Response(utils.data_to_csv_string(data), mimetype='text/csv')
+
+@app.route('/api/hackerquery/')
+def get_hack_by_name():
+    who = request.args.get('name')
+    q = Items.query.filter(Item.name == who).all()
+    data = [['Name', 'Location', 'Prize', 'Times Seen']] +\
+           [[str(d.name), str(d.location), str(d.prize), get_seen_ct(d.id)] for d in q]
     return Response(utils.data_to_csv_string(data), mimetype='text/csv')
