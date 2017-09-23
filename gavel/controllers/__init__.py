22224fa31527f6c02be14444fe4c74a619d88c30
Heman Gandhi <hemangandhi@gmail.com>
2017-09-23 14:14:10 -0400

    starting from scratch

--- gavel/controllers/__init__.py
+++
@@ -1,7 +0,0 @@
-import gavel.controllers.default
-import gavel.controllers.csrf_protection
-import gavel.controllers.admin
-import gavel.controllers.api
-import gavel.controllers.judge
-import gavel.controllers.error
-import gavel.controllers.hacker_view
