edb041821633b52e36fb1208650d09e4aae85a56
Heman Gandhi <hemangandhi@gmail.com>
2017-09-27 23:42:30 -0400

    sqlite

--- sledge/models/__init__.py
+++ sledge/models/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-db = create_engine('sqlite:///../../sledge.db')
+db = create_engine('sqlite:///sledge.db', echo=True)
 Model = declarative_base()
 metadata = MetaData()
 
