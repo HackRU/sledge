5bf75f6cb807c2d1fdccac756fa6ed299fe46552
mjrb <www.w.com123@gmail.com>
2018-03-22 21:52:32 -0400

    add judge allocation alg and some stageing to test

---
+++ server/allocation.py
@@ -0,0 +1,49 @@
+'''utilities for allocating judges'''
+import math
+import copy
+
+def allocate_judges(judges, hacks, judges_per_hack):
+    '''
+    returns ranges of which hack should goto which judge
+    so that each hack gets judges_per_hack amount of judges
+    '''
+    partition_size = hacks // judges
+    partitions_per_judge = judges_per_hack
+    hack_nums=list(range(0,hacks))
+    partitions=partition(hack_nums, partition_size)
+    # partitions go around in a circle stealing elements
+    # from eachother until the list has as many partitions
+    # as judges
+    theif=0
+    victim=1
+    while len(partitions) > judges:
+        partitions[theif].append(copy.deepcopy(partitions[victim][0]))
+        del partitions[victim][0]
+        if len(partitions[victim]) == 0:
+            del partitions[victim]
+        theif+=1
+        if theif==len(partitions)-1:
+            theif=0
+        victim=theif + 1
+        
+    allocations=[]
+    for i in range(0,judges):
+        for j in range(i, i + partitions_per_judge):
+            try:
+                allocations[i]+=partitions[j % len(partitions)]
+            except Exception as e:
+                allocations.append(copy.deepcopy(partitions[j % len(partitions)]))
+    return allocations
+
+def partition(nums, partition_size):
+    '''
+    splits nums into arrays of size partition
+    if it doesn't devide evenly the remander is added to the end
+    '''
+    partitions=[]
+    num_partitions = math.ceil(len(nums) / partition_size)
+    # +2 because range goes to len -1 and i need to goto len+1
+    # to also get the remander
+    for i in range(0, num_partitions*partition_size, partition_size):
+        partitions.append(copy.deepcopy(nums[i:i+partition_size]))
+    return partitions
---
+++ server/testallocation.py
@@ -0,0 +1,23 @@
+import allocation
+def test(judges,hacks,judges_per_hack):
+    the_allocation=allocation.allocate_judges(judges,hacks,judges_per_hack)
+    # maybe check for std deviation to make sure distribution is "fair"
+    # maybe check range instead
+    assert len(the_allocation)==judges
+    judgings=[0]*hacks
+    hacks_per_judge=[0]*judges
+    judge_num=0
+    for judge in the_allocation:
+        assert judge_num<judges # cant have more allocations then judges
+        hacks_per_judge[judge_num]=len(judge)
+        for hack in judge:
+            assert hack<hacks # cant judge a hack that doesn't exist
+            judgings[hack]+=1
+        judge_num+=1
+    for times_judged in judgings:
+        assert times_judged==judges_per_hack
+    max_hacks_per_judge=max(hacks_per_judge)
+    min_hacks_per_judge=min(hacks_per_judge)
+    print("min: "+str(min_hacks_per_judge)+" max: "+str(max_hacks_per_judge))
+    print("range: "+str(max_hacks_per_judge-min_hacks_per_judge))
+    print(hacks_per_judge)
