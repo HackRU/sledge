926372c127a4ac0b118b79383235e821014835ec
Eric Roberts <eric@threedot14.com>
2018-09-24 21:31:52 -0400

    Fix deploy script stuff

--- docs/Deployment.md
+++ docs/Deployment.md
@@ -28,12 +28,61 @@ far in advance.
 
 ## Software
 
-Sledge should be installed on Debian 9 x64. Run the `deploy-debian.sh` as root
-from a fresh Debian 9 installation *after* the DNS propagates. If you run the
-script too early, the nginx config will fail and Sledge won't be able to start.
-In this case you can rerun the cerbot command from the script and restart the
-server. This should be done with nginx stopped.
+Sledge should be installed on Debian 9 x64. Run the `deploy-debian.sh` script as
+root from a fresh Debian 9 installation *after* the DNS propagates. If you run
+the script too early, the nginx config will fail and Sledge won't be able to
+start.  In this case you can rerun the cerbot command from the script and
+restart the server. This should be done with nginx stopped.
+
+After the script runs, restart the server.
 
 ## Setup
 
-**TODO**
+The script from the previous step should have created a `admin-token.txt` file
+in the directory it was created. Go to `setup.html` and, under Authentication,
+enter this token in the "secret" field. Then click "Set", "Auto: Off" (so it
+becomes "Auto: On") and refresh the page. Check the Javascript console for
+errors. After each action you take, be sure to check the Javascript console.
+
+Under Manually Add Data add the following 4 categories:
+
+ * Creativity
+ * Functionality
+ * Technical Difficulty
+ * Design
+
+Judges must also be added manually.
+
+Then, follow the instructions to import from Devpost.
+
+Click "Auto Assign Tables" and alter table placements as necessary. Note there
+is nothing to preventing multiple projects from having the same table, although
+this is unwise so try not to do this.
+
+Judges must be assigned after tables are assigned. Assignments should be clear
+before using "Auto Assign 3 Judges Per Hacks". Once judging starts, do not
+automatically reassign judges.
+
+## Instructions for Judges
+
+Judges log in from `login.html`. They simply need to select their name. Then,
+they should go to `judge.html` to start
+
+## Instructions for Hackers
+
+The only page that should concern hackers is `tables.html` which will show them
+a full list of tables. If the server starts performing slowly, it might be wise
+to copy this page to a spreadsheet, then disable the live version.
+
+## What to do if...
+
+### I need to add a Hack after judging starts
+
+*Do not* auto-reassign judges after judging starts.
+
+*Do not* use the "Import from Devpost" function more than once.
+
+*Do* Add the Hack manually, then assign judges manually using the matrix. The
+priority (number in the matrix) represents the order the judges will go though
+hacks. When assigning manually, just choose a number higher than the judge's
+current priorities (ex. 1000), and increase if there's more hacks.
--- tools/deploy-debian9.sh
+++ tools/deploy-debian9.sh
@@ -14,12 +14,12 @@ apt-get -y dist-upgrade
 apt-get -y install apt-transport-https lsb-release
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 curl -sS https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-echo "deb https://deb.nodesource.com/node_8.x $(lsb_release -c -s) main" >/etc/apt/sources.list.d/nodesources.list
+echo "deb https://deb.nodesource.com/node_10.x $(lsb_release -c -s) main" >/etc/apt/sources.list.d/nodesources.list
 echo "deb https://dl.yarnpkg.com/debian/ stable main" >/etc/apt/sources.list.d/yarn.list
 
 # Update repos and install additional packages
 apt-get -y update
-apt-get -y install nodejs yarn build-essential git sudo certbot nginx
+apt-get -y install nodejs yarn build-essential git sudo certbot nginx xxd
 
 # nginx automatically starts when it's installed, we don't want that yet
 service nginx stop
@@ -29,11 +29,13 @@ certbot certonly -n --standalone --agree-tos --email eric@threedot14.com --domai
   echo "================================================"
   echo "===== WARNING: Could not get certificates! ====="
   echo "================================================"
+  echo "The server will continue to be created with a self-signed certificate"
 }
 
 # Setup iptables to block most incoming connections,
 # after first deleting the current config
-#  Note: We don't care about FORWARD, and we leave INPUT as accept
+#  Note: We don't care about FORWARD, and we leave INPUT as accept until the
+#  other rules are there
 iptables -P INPUT ACCEPT
 iptables -F INPUT
 iptables -P OUTPUT ACCEPT
@@ -44,6 +46,17 @@ iptables -A INPUT -p tcp --match multiport --dports 22,80,443 -j ACCEPT
 
 iptables -P INPUT DROP
 
+# Sledge, particularly the build process, should have at least 1GB of RAM.
+# Although our delpoyment server *should* be equipped with much more, we create
+# a swapfile to prevent nasty EOMs that are difficult to track down.
+if [ ! -f /swapfile ]; then
+  dd if=/dev/zero of=/swapfile bs=1024 count=1000000
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+  echo -e "/swapfile\tnone\tswap\tsw\t0\t0" >>/etc/fstab
+fi
+
 # Create user for sledge
 useradd -mr sledge
 
@@ -52,6 +65,7 @@ sudo -u sledge sh - <<EOF
 cd \$HOME
 git clone https://github.com/HackRU/sledge.git sledge
 cd sledge
+git checkout fall2018
 
 yarn install
 ./gulp build
@@ -63,16 +77,48 @@ tee /usr/local/bin/sledge-daemon <<EOF
 cd /home/sledge/sledge
 sudo -u sledge ./sledge 4000
 EOF
+chmod 755 /usr/local/bin/sledge-daemon
 
 # Add and enable sledge service
-tee /etc/systemd/system/sledge.service <<EOF
+tee /lib/systemd/system/sledge.service <<EOF
 [Unit]
 Description=A Judging System for Hackathons
 
 [Service]
-ExecStart=sledge-daemon
+ExecStart=/usr/local/bin/sledge-daemon
 EOF
 service sledge enable
+sleep 3 # Wait for Sledge to fully start
+
+# Add an initial admin token
+ADMIN_TOKEN="$(head -c5 </dev/random | xxd -p)"
+echo $ADMIN_TOKEN >admin-token.txt
+sqlite3 /home/sledge/sledge/sledge.db "INSERT INTO Token(\"$ADMIN_TOKEN\", 0);"
+
+# Sledge ssl certificate
+mkdir -p /etc/nginx/ssl
+ln -s /etc/letsencrypt/live/sledge.site/privkey.pem /etc/nginx/ssl/sledge.key
+ln -s /etc/letsencrypt/live/sledge.site/fullchain.pem /etc/nginx/ssl/sledge.cert
+tee /etc/nginx/snippets/ssl-sledge <<EOF
+ssl_certificate /etc/letsencrypt/live/sledge.site/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/sledge.site/privkey.pem;
+EOF
+
+# Self-signed certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.cert \
+  -subj "/CN=netbuzz.xyz"
+tee /etc/nginx/snippets/ssl-nginx <<EOF
+ssl_certificate /etc/nginx/ssl/nginx.cert;
+ssl_certificate_key /etc/nginx/ssl/nginx.key;
+EOF
+
+# Choose which certificate, based on if we got the certificate
+if [ -d "/etc/letsencrypt/live/sledge.site" ]; then
+  SSL_SNIPPET="snippets/ssl-sledge"
+else
+  SSL_SNIPPET="snippets/ssl-nginx"
+fi
 
 # Add nginx config for website
 tee /etc/nginx/sites-available/sledge <<EOF
@@ -82,18 +128,17 @@ server {
 
   server_name www.sledge.site;
 
-  ssl_certificate /etc/letsencrypt/live/sledge.site/fullchain.pem
-  ssl_certificate_key /etc/letsencrypt/live/sledge.site/privkey.pem
+  include $SSL_SNIPPET;
 
   root /var/www/html;
   index index.html;
 
   location /socket.io {
-    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header Connection "upgrade";
     proxy_http_version 1.1;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header Host $host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header Host \$host;
     proxy_pass http://localhost:4000;
   }
 
@@ -109,24 +154,19 @@ server {
   listen 443 ssl;
   listen [::]:443 ssl;
 
-  ssl_certificate /etc/letsencrypt/live/sledge.site/fullchain.pem
-  ssl_certificate_key /etc/letsencrypt/live/sledge.site/privkey.pem
+  include $SSL_SNIPPET;
 
-  return 301 https://www.sledge.site$request_uri;
+  return 301 https://www.sledge.site\$request_uri;
 }
 EOF
 ln -s /etc/nginx/sites-available/sledge /etc/nginx/sites-enabled/sledge
-nginx -t || {
-  echo "=============="
-  echo "== WARNING! =="
-  echo "=============="
-  echo
-  echo "The nginx config provided is not valid. This is probably because certbot failed to get the proper certificates."
-}
+
+# Test nginx config
+nginx -t
 
 # Finish message
 echo "============================"
 echo "== Sledge Setup Complete! =="
 echo "============================"
-echo
 echo "The setup script has finished installing Sledge. Please see documentation for what to do next."
+echo "Your admin token (also stored in admin-token.txt): $ADMIN_TOKEN"
