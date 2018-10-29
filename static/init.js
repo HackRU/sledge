367368d08b3423fa1db831b7374374bb3d0787ea
Eric Roberts <eric@threedot14.com>
2018-10-29 15:18:25 -0400

    Update dependencies

--- static/init.js
+++ static/init.js
@@ -1,7 +1,7 @@
 (function () {
   var script = document.createElement("script");
   script.type = "text/javascript";
-  script.src = "https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.21.4/system.js";
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.21.5/system.js";
   document.head.appendChild(script);
 
   function fixDefaultExportProblem(internalModuleName, externalModuleName) {
@@ -37,15 +37,15 @@
 
       map: {
         "socket.io-client": "https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.dev.js",
-        "_react": "https://cdnjs.cloudflare.com/ajax/libs/react/16.4.1/umd/react.development.js",
-        "_react-dom": "https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.4.1/umd/react-dom.development.js",
-        "react-redux": "https://cdnjs.cloudflare.com/ajax/libs/react-redux/5.0.7/react-redux.js",
-        "redux": "https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.0/redux.js",
-        "reactstrap": "https://cdnjs.cloudflare.com/ajax/libs/reactstrap/6.1.0/reactstrap.full.js",
+        "_react": "https://cdnjs.cloudflare.com/ajax/libs/react/16.4.2/umd/react.development.js",
+        "_react-dom": "https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.4.2/umd/react-dom.development.js",
+        "react-redux": "https://cdnjs.cloudflare.com/ajax/libs/react-redux/5.1.0/react-redux.js",
+        "redux": "https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.1/redux.js",
+        "reactstrap": "https://cdnjs.cloudflare.com/ajax/libs/reactstrap/6.5.0/reactstrap.full.js",
         "_immutable": "https://cdnjs.cloudflare.com/ajax/libs/immutable/3.8.2/immutable.js",
-        "_papaparse": "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.5.0/papaparse.js",
+        "_papaparse": "https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.6.1/papaparse.js",
         "mocha": "https://cdnjs.cloudflare.com/ajax/libs/mocha/5.2.0/mocha.min.js",
-        "chai": "https://cdnjs.cloudflare.com/ajax/libs/chai/4.1.2/chai.js"
+        "chai": "https://cdnjs.cloudflare.com/ajax/libs/chai/4.2.0/chai.js"
       },
       meta: {
         "socket.io-client": { format: "amd" },
