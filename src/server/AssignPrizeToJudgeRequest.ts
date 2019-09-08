8cdd3734cf06cc0f836108ba0aca3fa78e0bfd10
Eric Roberts <eric@threedot14.com>
2019-09-07 21:24:27 -0400

    Add ranking assignment when assigning prize to judge

--- src/server/AssignPrizeToJudgeRequest.ts
+++ src/server/AssignPrizeToJudgeRequest.ts
@@ -14,16 +14,68 @@ export class AssignPrizeToJudgeRequest implements RequestHandler {
   }
 
   handleSync(data: object): object {
+    const judgeId: number = data["judgeId"];
+    const prizeId: number = data["prizeId"];
+
+    this.db.begin();
+    // First, create assignments for all relevant submissions which they haven't rated
     const requiredSubmissions = this.db.prepare(
       "SELECT submissionId FROM SubmissionPrize WHERE prizeId=? "+
         "EXCEPT "+
         "SELECT submissionId FROM RatingAssignment "+
           "LEFT JOIN Assignment ON RatingAssignment.assignmentId=Assignment.id "+
           "WHERE judgeId=?;"
-    ).all(data["judgeId"], data["prizeId"]);
+    ).all(prizeId, judgeId);
 
-    console.log(requiredSubmissions.map(x => x.submissionId));
+    // Get next assignment priority
+    const highestPriorityAssignment = this.db.prepare(
+      "SELECT priority FROM Assignment WHERE judgeId=? ORDER BY priority DESC;"
+    ).get(judgeId);
+    let priority;
+    if (highestPriorityAssignment) {
+      priority = highestPriorityAssignment.priority + 1;
+    } else {
+      priority = 1;
+    }
 
-    return {error: "NYI"};
+    // Create rating assignments
+    for (let rs of requiredSubmissions) {
+      const submissionId = rs.submissionId;
+      const assignmentId = this.db.prepare(
+        "INSERT INTO Assignment(judgeId, priority, type, active) "+
+          "VALUES(?, ?, 1, 1);"
+      ).run(judgeId, priority).lastInsertRowid;
+      this.db.prepare(
+        "INSERT INTO RatingAssignment(assignmentId, submissionId) "+
+          "VALUES(?, ?);"
+      ).run(assignmentId, submissionId);
+
+      priority++;
+    }
+
+    // Now create the ranking assignment
+    const newAssignmentId = this.db.prepare(
+      "INSERT INTO Assignment(judgeId, priority, type, active) "+
+        "VALUES(?, ?, 2, 1);"
+    ).run(judgeId, priority).lastInsertRowid;
+    const rankingAssignmentId = this.db.prepare(
+      "INSERT INTO RankingAssignment(assignmentId, prizeId) "+
+        "VALUES(?, ?);"
+    ).run(newAssignmentId, prizeId).lastInsertRowid;
+
+    // Add the empty rankings for each submission
+    const submissionsForRanking = this.db.prepare(
+      "SELECT submissionId FROM SubmissionPrize WHERE prizeId=?;"
+    ).all(prizeId);
+    for (let sfr of submissionsForRanking) {
+      const submissionId = sfr.submissionId;
+      this.db.prepare(
+        "INSERT INTO Ranking(rankingAssignmentId, submissionId) "+
+          "VALUES(?, ?);"
+      ).run(rankingAssignmentId, submissionId);
+    }
+    this.db.commit();
+
+    return {success: true};
   }
 }
