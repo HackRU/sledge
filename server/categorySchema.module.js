1688da3acd05956bf71b9b4507bcf7b1a50af302
Sam Ping <samuelping42@gmail.com>
2021-03-11 14:07:52 -0500

    Add category schema

---
+++ server/categorySchema.module.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+const categorySchema = new Schema({
+    categoryID: String,
+    name: String, // category name
+    companyName: String, // null if prize is not company-sponsored
+    category: String // track/superlative
+})
+
+const category = mongoose.model("category", categorySchema);
+module.exports = category;
