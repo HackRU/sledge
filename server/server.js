45c77587cfa01ed5169eb1b8de07b76b46223f97
Sam Ping <samuelping42@gmail.com>
2021-03-16 12:37:24 -0400

    Fix server.js

--- server/server.js
+++ server/server.js
@@ -13,27 +13,4 @@ mongoose.connection
     console.warn("Error : ", error);
   });
 
-// returns all submissions
-app.get("/api/submissions", (req, res) => {
-  res.status(200).send(submission.find({}));
-});
-
-// return submission data
-app.get("/api/submissions/:teamID", (req, res) => {
-  res.status(200).send(submission.findById(req.params.teamID));
-});
-
-// add/update submission
-app.post("/api/submissions/:teamID", (req, res) => {
-  submission.findByIdAndUpdate(req.params.teamID, req.body);
-  res.status(200);
-});
-
-app.post("/login", (req, res) => {
-  // 1. get hacker's teamID from LCS
-  // 2. log them in
-  // 3. ???
-  // 4. profit
-});
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
