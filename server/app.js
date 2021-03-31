1ab32bac2a698f2dff92d0743128ca279602721b
Sam Ping <samuelping42@gmail.com>
2021-03-31 12:58:52 -0400

    Code split routes - Split submissions routes away from app.js routes

--- server/app.js
+++ server/app.js
@@ -1,32 +1,11 @@
 const express = require("express");
-const { update } = require("./models/submissionSchema.model");
 
 const app = express();
 app.use(express.json());
 
-const Submission = require("./models/submissionSchema.model");
+const submissionsRouter = require("./routes/submissions");
 
-// returns all submissions
-app.get("/api/submissions", async (req, res) => {
-  res.status(200).send(await Submission.find({}));
-});
-
-// return submission data
-app.get("/api/submissions/:teamID/:submissionID", async (req, res) => {
-  await Submission.findById(req.params.submissionID, (err, submission) => {
-    if (err) res.status(500).send(err);
-    res.status(200).send(submission);
-  });
-});
-
-// create new submission
-app.post("/api/submissions/:teamID/create", (req, res) => {
-  const currentSubmission = new Submission(req.body);
-  currentSubmission.save((err, submission) => {
-    if (err) res.status(500).send(err);
-    res.status(200).send(submission);
-  });
-});
+app.use("/api/submissions", submissionsRouter);
 
 app.post("/login", (req, res) => {
   // 1. get hacker's teamID from LCS
@@ -34,18 +13,5 @@ app.post("/login", (req, res) => {
   // 3. ???
   // 4. profit
 });
-
-// Update isSubmitted to true
-app.patch("/api/submissions/:teamID/:submissionID/submit", async (req, res) => {
-  await Submission.findByIdAndUpdate(req.params.submissionID, {isSubmitted: true});
-  res.status(200).send(await Submission.findById(req.params.submissionID));
-});
-
-// Update isSubmitted to false
-app.patch("/api/submissions/:teamID/:submissionID/unsubmit", async (req, res) => {
-  await Submission.findByIdAndUpdate(req.params.submissionID, {isSubmitted: false});
-  res.status(200).send(await Submission.findById(req.params.submissionID));
-});
-
 
 module.exports = app;
