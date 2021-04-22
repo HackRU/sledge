329384c30d5103c46759a0ed61636f7fca7e86ff
Sam Ping <samuelping42@gmail.com>
2021-04-22 04:46:37 -0400

    Update codedeploy instructions to run reload_server script; Update reset_directory script to not delete whole directory and recreate it, but instead just delete everything inside directory

--- scripts/reset_directory.sh
+++ scripts/reset_directory.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rm -rf /var/www/sledge # delete directory and everything inside it
-mkdir /var/www/sledge # recreate directory
+rm -rf /var/www/sledge/* # delete everything inside directory
