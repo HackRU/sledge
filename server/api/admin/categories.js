544650276953bf14b8eaea85621d8ebd7b40e24b
Jason Garcia <jasong2000@gmail.com>
2021-05-12 01:32:21 -0400

    Fixed some errors with passing tests

--- server/api/admin/categories.js
+++ server/api/admin/categories.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-const router = require("express").Router();
-const Category = require("../../models/categorySchema.model");
-=======
 const router = require('express').Router();
-// const Category = require('../../models/categorySchema.model');
+const Category = require('../../models/categorySchema.model');
 
 // Create a new category
 router.post("/create", async (req, res) => {
