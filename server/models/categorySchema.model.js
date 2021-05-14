068db4d0f5c12beec0acd72673f5f03f76f57ed9
Jason Garcia <jasong2000@gmail.com>
2021-05-14 01:36:49 -0400

    updated the category schema

--- server/models/categorySchema.model.js
+++ server/models/categorySchema.model.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const categorySchema = new Schema({
   name: String, // category name
   companyName: String, // null if prize is not company-sponsored
-  trackOrSuperlative: String, // track/superlative
+  type: String, // track/superlative
 });
 
 module.exports = mongoose.model('category', categorySchema);
