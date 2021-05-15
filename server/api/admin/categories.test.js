34280468ec7d29e4f670f4edbc5cf60c15556643
Jason Garcia <jasong2000@gmail.com>
2021-05-15 15:05:44 -0400

    Jason branch (#198)
    * Added a test cases for categories.test.js
    
    * Implemented categories endpoints
    
    * updated the category schema
    
    * Fixed linting problems
    
    * added new delete endpoint
    
    * cleaned up the update function

--- server/api/admin/categories.test.js
+++ server/api/admin/categories.test.js
@@ -1,9 +1,14 @@
-// const supertest = require('supertest');
+const supertest = require('supertest');
 const mongoose = require('mongoose');
-// const app = require('../../index');
+const app = require('../../index');
 const config = require('../../config.json');
+const Category = require('../../models/categorySchema.model');
 
-// const request = supertest(app);
+const request = supertest(app);
+
+const testCategory = require('../../testCategory.json');
+
+let testCategoryId;
 
 beforeAll(async () => {
   const url = `mongodb://${config.dbHost}:${config.dbPort}/${config.testDbName}`; // Connection URL, set it in config.json
@@ -15,8 +20,82 @@ beforeAll(async () => {
 });
 
 describe('testing category endpoints', () => {
-  it('is a sample test', () => {
-    expect(1).toEqual(1);
+  it('creates a new category', async (done) => {
+    const res = await request
+      .post('/api/admin/categories/create')
+      .send(testCategory);
+    expect(res.statusCode).toEqual(200);
+
+    testCategoryId = res.body.id; // res returns the id of the inserted submission
+
+    await Category.findById(testCategoryId, (err, category) => {
+      expect(category).not.toBeNull();
+    });
+    done();
+  });
+
+  it('returns all categories', async (done) => {
+    const res = await request.get('/api/admin/categories').send();
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).not.toBeNull();
+    done();
+  });
+
+  it('returns a category with specific ID', async (done) => {
+    const res = await request
+      .get(`/api/admin/categories/${testCategoryId}`)
+      .send(testCategory);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.id).toEqual(testCategoryId);
+    done();
+  });
+
+  it('updates a category with a specific ID', async (done) => {
+    const updatedFields = {
+      name: 'Sample Updated Name',
+      companyName: 'Sample Company Name',
+      type: 'superlative',
+    };
+
+    const res = await request
+      .patch(`/api/admin/categories/${testCategoryId}`)
+      .send(updatedFields);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.id).toEqual(testCategoryId);
+
+    await Category.findById(testCategoryId, (err, category) => {
+      expect(category.name).toEqual(updatedFields.name);
+      expect(category.companyName).toEqual(updatedFields.companyName);
+      expect(category.type).toEqual(updatedFields.type);
+    });
+
+    done();
+  });
+
+  it('deletes a single category with a specific ID', async (done) => {
+    const res = await request.delete(
+      `/api/admin/categories/delete/${testCategoryId}`,
+    );
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.message).toEqual('success');
+
+    await Category.findById(testCategoryId, (err, category) => {
+      expect(category).toBeNull();
+    });
+
+    await request.post('/api/admin/categories/create').send(testCategory);
+    done();
+  });
+
+  it('deletes all categories', async (done) => {
+    const res = await request.delete('/api/admin/categories/delete').send();
+    expect(res.statusCode).toEqual(200);
+
+    await Category.count({}, (err, count) => {
+      expect(count).toEqual(0);
+    });
+
+    done();
   });
 });
 
