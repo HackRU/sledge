08b459b216175bbc96b732e3679390eb27a530f2
Sam Ping <samuelping42@gmail.com>
2021-05-29 12:45:52 -0400

    Add sample component and frontend-core stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import { CoreProvider } from "@hackru/frontend-core";
+// import LINKER from './Linker';
+// import CONFIG from './Config';
+
+import Button from './components/Button';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Sledge!</h1>
+      <Button
+        name="Jason"
+        message="Sledge is the coolest web application ever!"
+      />
     </div>
   );
 }
---
+++ client/src/Linker.js
@@ -0,0 +1,14 @@
+/**
+ * Our frontend-core linker file
+ */
+
+const LINKER = {
+  // "MODULE-F": Test,
+  // "MODULE-G": Login,
+  // "MODULE-H": TestLogin,
+  // "MODULE-I": TestLogout,
+  // "MODULE-J": TestGetUser,
+  // "MODULE-K": TestUserStore,
+};
+
+export default LINKER;
