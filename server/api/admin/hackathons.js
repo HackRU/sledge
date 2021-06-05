a8a956758cc18d35ed845fb733e3b16314a4b9b8
Jason Garcia <jasong2000@gmail.com>
2021-06-05 12:01:35 -0400

    Wrote some of the endpoints

--- server/api/admin/hackathons.js
+++ server/api/admin/hackathons.js
@@ -1,16 +1,102 @@
 const router = require('express').Router();
 const Hackathon = require('../../models/hackathonSchema.model');
 
-router.post('/', (req, res) => {});
+/**
+ * @swagger
+ *
+ */
+router.post('/', async (req, res) => {
+  Hackathon.create({}, (err, hackathon) => {
+    if (err) res.status(500).send(err);
+    res.status(200).json({
+      message: 'success',
+      id: category.id,
+    });
+  });
+});
 
-router.patch('/:hackathonId', (req, res) => {});
+/**
+ * @swagger
+ *
+ */
+router.patch('/:hackathonId', async (req, res) => {
+  const hackathonToUpdate = await Hackathon.findById(req.params.hackathonId);
+  if ('season' in req.body) {
+    hackathonToUpdate.season = req.body.season;
+  }
 
-router.get('/', (req, res) => {});
+  if ('categories' in req.body) {
+    hackathonToUpdate.categories = req.body.categories;
+  }
 
-router.get('/current', (req, res) => {});
+  if ('submissionPhase' in req.body) {
+    if ('inProgress' in req.body.submissionPhase) {
+        hackathonToUpdate.submissionPhase.inProgress = req.body.submissionPhase.inProgress;
+    }
+    if ('deadline' in req.body.submissionPhase) {
+        hackathonToUpdate.submissionPhase.deadline = req.body.submissionPhase.deadline;
+    }
+    if ('flags' in req.body.submissionPhase) {
+        hackathonToUpdate.submissionPhase.flags = req.body.submissionPhase.flags;
+    }
+  }
 
-router.get('/:hackathonId', (req, res) => {});
+  if ('judgingPhase' in req.body) {
+    if ('inProgress' in req.body.judgingPhase) {
+        hackathonToUpdate.judgingPhase.inProgress = req.body.judgingPhase.inProgress;
+    }
+  }
 
-router.get('/:hackathonId/phase', (req, res) => {});
+  if ('isComplete' in req.body) {
+    hackathonToUpdate.isComplete = req.body.isComplete;
+  }
+
+  hackathonToUpdate.save();
+  res.status(200).json({
+      message: 'success',
+      id: req.params.hackathonId,
+  });
+});
+
+/**
+ * @swagger
+ *
+ */
+router.get('/', async (req, res) => {
+  res.status(200).send(await Hackathon.find({}));
+});
+
+/**
+ * @swagger
+ *
+ */
+router.get('/current', async (req, res) => {
+  res.status(200).send(await Hackathon.findOne({ isComplete: false }));
+});
+
+/**
+ * @swagger
+ *
+ */
+router.get('/:hackathonId', async (req, res) => {
+  res.status(200).send(await Hackathon.findById(req.params.hackathonId));
+});
+
+/**
+ * @swagger
+ *
+ */
+router.get('/:hackathonId/phase', async (req, res) => {
+  const selectedHackathon = await Hackathon.findById(req.params.hackathonId);
+  if (!selectedHackathon.submissionPhase.isComplete) {
+    res.status(200).send('submission');
+  } else if (!selectedHackathon.judgingPhase.isComplete) {
+    res.status(200).send('judging');
+  } else if (!selectedHackathon.isComplete) {
+    res.status(200).send('completed');
+  } else {
+    res.status(200).send('not started');
+  }
+});
 
 module.exports = router;
