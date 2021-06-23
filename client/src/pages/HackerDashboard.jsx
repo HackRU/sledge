b87618982529c76f36023a47a1bc918500c8230b
Deep310 <dparekh2019@gmail.com>
2021-06-23 14:49:49 -0400

    built hacker dashboard

--- client/src/pages/HackerDashboard.jsx
+++ client/src/pages/HackerDashboard.jsx
@@ -5,7 +5,10 @@ import styles from './HackerDashboard.module.scss';
 function HackerDashboard() {
   return (
     <div className={styles.hackerDashboardContainer}>
-      <p>Your past submissions</p>
+      <p>Make a new submission:</p>
+      <div className={styles.newSubmission}>
+        <a href="upload" className={styles.btn}>+ Upload a new submission</a></div>
+      <p>Your past submissions:</p>
       <PastSubmissions />
     </div>
   );
