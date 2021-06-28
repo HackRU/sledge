de59963af0a00f933f91ac757a9a1ea1d0135ac6
Samuel Ping <55605862+samuel-ping@users.noreply.github.com>
2021-06-27 22:55:42 -0400

    Add routing (#219)
    - Added basic routing to pages for now
    - Deleted useless files

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,46 @@
 // import LINKER from './Linker';
 // import CONFIG from './Config';
 
-import Button from './components/SampleButton';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
 import HackerDashboard from './pages/HackerDashboard';
 
 import './App.css';
 
 function App() {
+  // Use profile store to see if user is logged in. If not, redirect them to hackru login.
+  // if (true) {
+  //   window.location.href = 'https://hackru.org';
+  // }
+
+  // Use <Redirect /> from react-router in a if statement to see if user is admin, hacker, or judge.
   return (
-    <div className="App">
-      <h1>Welcome to Sledge!</h1>
-      <Button
-        name="Jason"
-        message="Sledge is the coolest web application ever!"
-      />
-      <HackerDashboard />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          {/* For now, base route shows list of links to go to. */}
+          <Route exact path="/">
+            <ul>
+              <li>
+                <Link to="/hacker">Go to hacker dashboard</Link>
+              </li>
+            </ul>
+          </Route>
+          <Route exact path="admin">
+            {/* Admin Dashboard Component */}
+          </Route>
+          <Route exact path="/hacker">
+            <HackerDashboard />
+          </Route>
+          <Route exact path="judge">
+            {/* Judge Dashboard Component */}
+          </Route>
+          <Route exact path="/hacker/submission">
+            {/* Submission Form Component */}
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Config from './config.json';
 
 ReactDOM.render(
   <React.StrictMode>
