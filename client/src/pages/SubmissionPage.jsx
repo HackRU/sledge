bf1f05cdcb76d5fe60c65f78afcd058f3f588982
Jason Garcia <jasong2000@gmail.com>
2021-06-28 21:41:45 -0400

    Staged more changes

---
+++ client/src/pages/SubmissionPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SubmissionForm from '../components/SubmissionForm';
+
+function SubmissionPage() {
+    return (
+        <div>
+            <h1>Submission Page</h1>
+            <SubmissionForm/>
+        </div>
+    );
+}
+
+export default SubmissionPage;
