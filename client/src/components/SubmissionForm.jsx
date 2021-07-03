317f8e8767c835b1af2af3f9007bcbce0de73825
Jason Garcia <jasong2000@gmail.com>
2021-07-03 11:26:37 -0400

    Fixed some errors

--- client/src/components/SubmissionForm.jsx
+++ client/src/components/SubmissionForm.jsx
@@ -6,10 +6,6 @@ const useStyles = makeStyles((theme) => ({
     root: {
       width: '100%',
     },
-    TextField: {
-        width: 200,
-        color: 'blue',
-    }
   }));
   
 
@@ -21,12 +17,12 @@ export default function SubmissionForm() {
     }
     return (
         <div className={classes.root}>
-            <TextField classes={classes.TextField} id="titleTxt" label="Title:" /><br/>
-            <TextField classes={classes.TextField} id="techTxt" label="Technologies:"/><br/>
-            <TextField classes={classes.TextField} id="descTxt" label="Description:" multiline="true" rows="5" /><br/>
-            <TextField classes={classes.TextField} id="urlTxt" label="URLs:" multiline="true" rows="5"/><br/>
-            <TextField classes={classes.TextField} id="imagesTxt" label="Image:"/> <br/>
-            <TextField classes={classes.TextField} id="categoriesTxt" label="Categories:"/><br/> <br/>
+            <TextField id="titleTxt" label="Title:" /><br/>
+            <TextField id="techTxt" label="Technologies:"/><br/>
+            <TextField id="descTxt" label="Description:" multiline="true" rows="5" /><br/>
+            <TextField id="urlTxt" label="URLs:" multiline="true" rows="5"/><br/>
+            <TextField id="imagesTxt" label="Image:"/> <br/>
+            <TextField id="categoriesTxt" label="Categories:"/><br/> <br/>
             <Button id="submitBtn" type="submit" onClick={() => {
                 print('You Submitted!! :D');
             }}>Submit</Button>
