deb727223853edc6af66fa99f069e7395ccc39cd
Jason Garcia <jasong2000@gmail.com>
2021-09-24 03:10:08 -0400

    Implemented the Hackathon form

--- client/src/components/HackathonForm.jsx
+++ client/src/components/HackathonForm.jsx
@@ -1,5 +1,6 @@
 import {
     Button,
+    Divider,
     FormLabel,
     Grid,
     IconButton,
@@ -8,6 +9,7 @@ import {
   import { makeStyles } from '@material-ui/core/styles';
   import DeleteIcon from '@material-ui/icons/Delete';
   import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import { mergeClasses } from '@material-ui/styles';
   import { Form, Formik, FieldArray } from 'formik';
   import * as yup from 'yup';
 
@@ -21,14 +23,13 @@ import {
       },
     },
     // Properly left aligns the "External Links" label for field where you can add multiple URLs.
-    urlFieldLabel: {
-      textAlign: 'start',
-      margin: theme.spacing(1),
+    categoryField: {
+      width: '50ch',
     },
-    labelField: {
+    companyField: {
       width: '35ch',
     },
-    urlField: {
+    typeField: {
       width: '50ch',
     },
     input: {
@@ -41,10 +42,122 @@ const validationSchema = yup.object({});
 export default function HackathonForm() {
     const classes = useStyles();
     return(
-        <Formik>
-          <Form>
-            
-          </Form>
+        <Formik
+          initialValues={{
+            season: "",
+            categories: [{
+              name: "",
+              companyName: "",
+              type: "",
+            }],
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({ values, handleChange, setFieldValue }) => (
+            <Form className={classes.root} autoComplete="off">
+              <TextField
+                name="season"
+                value={values.season}
+                onChange={handleChange}
+                label="Season"
+                helperText="Season of this Hackathon"
+                fullWidth
+                required
+              />
+
+              <FieldArray name="categories">
+                {({ push , remove }) => (
+                  <>
+                  <FormLabel className={classes.urlFieldLabel}>
+                    Categories
+                  </FormLabel>
+                  {values.categories.length === 0 ? (
+                  <FormLabel className={classes.urlFieldLabel}>
+                    Need At Least 1 Category!!!!!!
+                  </FormLabel>
+                ) : (
+                  <></>
+                )}
+                {/* eslint-disable-next-line */}
+                {values.categories.map((category, index) => {
+                  return (
+                    <div>
+                      <Grid
+                        container
+                        direction="row"
+                        justify="flex-start"
+                        alignItems="baseline"
+                      >
+
+                        <TextField
+                          className={classes.categoryField}
+                          name={`categories[${index}].name`}
+                          value={category.name}
+                          onChange={handleChange}
+                          label="Name"
+                          helperText="Name of the Category"
+                          required
+                        />
+
+                        <TextField
+                          className={classes.companyField}
+                          name={`categories[${index}].companyName`}
+                          value={category.companyName}
+                          onChange={handleChange}
+                          label="Company Name"
+                          helperText="Name of the company sponsoring the category (if Applicable)"
+                        />
+
+                        <TextField
+                          className={classes.typeField}
+                          name={`categories[${index}].type`}
+                          value={category.type}
+                          onChange={handleChange}
+                          label="Type"
+                          helperText="Type of the category, either 'Track' or 'Superlative'"
+                          required
+                        />
+
+                        <IconButton
+                          className={classes.deleteButton}
+                          aria-label="delete"
+                          color="secondary"
+                          onClick={() => remove(index)}
+                          edge="start"
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                        
+                      </Grid>
+                    </div>
+                  )
+                })}
+
+                <Button
+                  className={classes.button}
+                  type="button"
+                  onClick={() => push({ name: '', companyName: '', type: '',})}
+                >
+                  {values.categories.length === 0 ? 'Add link' : 'Add another link'}
+                </Button>
+                  </>
+                )}
+              </FieldArray>
+
+              <Button
+                name="submitButton"
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
+                Submit
+              </Button>
+
+            </Form>
+          )}
         </Formik>
     );
 }
