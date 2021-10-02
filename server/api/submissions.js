0dc0697d8c1084249ab8b7b3bbd5863a5ebf0433
5H1V <spatel0240@gmail.com>
2021-10-02 11:22:41 -0400

    updated error and status

--- server/api/submissions.js
+++ server/api/submissions.js
@@ -62,29 +62,29 @@ router.get('/:submissionID', async (req, res) => {
  *       - submissions
  */
 
- exports.create = (req, res) => {
-  if (!req.body.title) {
-    res.status(200).send(err);
-    return;
-  }
-  const submission = new Submission({
-    title: req.body.title,
-    description: req.body.description,
-    published: req.body.published ? req.body.published : false
-  });
-  submission
-    .save(submission)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(200).send({
-        message: err.message
+router.post('/', (req, res) => {
+  Submission.create({}, () => {
+    if (!req.body.title) {
+      res.status(400).send({message: "Content can not be empty!"});
+      return;
+    }
+    const submission = new Submission({
+      title: req.body.title,
+      description: req.body.description,
+      published: req.body.published ? req.body.published : false,
+    });
+    submission
+      .save(submission)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(200).send({
+          message: err.message,
+        });
       });
   });
-};
-
-
+});
 
 router.post('/', (req, res) => {
   Submission.create({}, (err, submission) => {
